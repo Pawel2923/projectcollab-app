5db606a84c99ee033a6a9cff78fa4909f59b3d56
Paweł Poremba <pawelporemba123@gmail.com>
2026-04-29 19:06:53 +0200

    wip: modify project files

--- setup-prod.sh
+++ setup-prod.sh
@@ -34,7 +34,5 @@ run_compose pull
 echo "Starting production containers..."
 run_compose up --wait --no-build
 
-echo "Generating new lexik JWT keys for production environment..."
-run_compose exec api php bin/console lexik:jwt:generate-keypair --overwrite --no-interaction
-
 echo "Production environment setup completed successfully."
+
