42ef35e65b09b36718750335d4400322c9ae9e69
Paweł Poremba <pawelporemba123@gmail.com>
2026-06-07 11:02:55 +0200

    feat(ui): remove static height from button

--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -25,9 +25,9 @@ const buttonVariants = cva(
         transparent: "bg-transparent",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-lg gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-lg px-6 has-[>svg]:px-4",
+        default: "px-4 py-2 has-[>svg]:px-3",
+        sm: "rounded-lg gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "rounded-lg px-6 has-[>svg]:px-4",
         icon: "size-9",
         dynamic: "",
       },
